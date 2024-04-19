dbad03c4b5458b58d29f8391cf87a775111a1fdd
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-19 19:37:10 +0500

    day 4 challenge

---
+++ index.ts
@@ -0,0 +1,31 @@
+                //🚀 Day 4 Challenge: Start Coding! 🚀//
+// Q10:Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+//If you don’t have anything specific to write because your programs are too simple at this point, 
+//just add your name and the current date at the top of each program file. Then write one sentence 
+//describing what the program does.
+
+//Program 1 // File 1
+//My name is Mehwish Naz
+//Dated: 19/02/1997
+console.log('Hellow world simple program');
+//Program 2 //File 2
+//My name is Mehwish Naz
+//Dated: 19/02/2024
+console.log(5*2);
+
+// Q11:Names: Store the names of a few of your friends in a array called names. Print each person’s 
+//name by accessing each element in the list, one at a time. //
+let members_names = ['sana', 'ayesha', 'nimra', 'kinza'];
+for (let i = 0; i < members_names.length; i++) 
+    {console.log(members_names[i]);}
+
+// Q12: Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s
+//name, print a message to them. The text of each message should be the same, but each message should
+//be personalized with the person’s name.//
+let names: string [] =["sana", "ayesha", "munza", "inza", "bisma"];
+let massage: string = "Do you like to read some books?"
+console.log(names[0] + " "+ massage);
+console.log(names[1]+ " " + massage);
+console.log(names[2]+ " " + massage);
+console.log(names[3]+ " " + massage);
+console.log(names[4]+ " " + massage); 
